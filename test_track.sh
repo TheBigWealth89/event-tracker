b0e012601886d7ca3ca0283a3a0ba20b0e46b644
Dabira Olaoluwa <dev373055@gmail.com>
2026-04-16 05:23:32 -0700

    test: add USER_C variable and corresponding login event curl requests to test script

--- test_track.sh
+++ test_track.sh
@@ -4,6 +4,23 @@ HOST="http://localhost:5000/track"
 URL="https://track.com"
 USER_A="user-123"
 USER_B="user-442"
+USER_C="user-1233"
+
+# Send a LOGIN event for User A
+echo "User A login event..."
+curl -X POST "$HOST" \
+  -H "Content-Type: application/json" \
+  -H "x-user-id: $USER_A" \
+  -d "{\"eventName\": \"login\", \"url\": \"$URL\", \"metadata\": { \"device\": \"desktop\", \"browser\": \"chrome\" }}"
+echo ""
+
+# Send a LOGIN event for User A
+echo "User C login event..."
+curl -X POST "$HOST" \
+  -H "Content-Type: application/json" \
+  -H "x-user-id: $USER_C" \
+  -d "{\"eventName\": \"login\", \"url\": \"$URL\", \"metadata\": { \"device\": \"desktop\", \"browser\": \"chrome\" }}"
+echo ""
 
 # Send a LOGIN event for User A
 echo "User A login event..."
